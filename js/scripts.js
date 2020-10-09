f2aa52bec43987c125fde32f560c243fb6da2209
FrancineU <uwizeyimanafrancine62@gmail.com>
2020-10-09 17:53:00 +0200

    add  click funtion

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,37 @@ $(document).ready(function () {
         $("#pizzaOrders").show();
   
     });
+    $("#add-order").click(function () {
+        $("#new-orders").append('<div class="new-order">' +
+            '<div class="form-group">' +
+            '<label for="new-size">Size</label>' +
+            '<select class="form-control new-size">' +
+            '<option>select size</option>' +
+            '<option>small</option>' +
+            '<option>medium</option>' +
+            '<option>large</option>' +
+            '</select>' +
+  
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="new-topping">topping</label>' +
+            '<select class="form-control new-topping">' +
+            '<option>select topping</option>' +
+            '<option>bacon</option>' +
+            '<option>mushrooms</option>' +
+            '<option>onions</option>' +
+            '</select>' +
+            '</div>' +
+  
+            '<div class="form-group">' +
+            '<label for="new-crust">Crust</label>' +
+            '<select class="form-control new-crust">' +
+            '<option value="" disabled selected>select crust</option>' +
+            '<option>crispy</option>' +
+            '<option>stuffed</option>' +
+            '<option>gluten-free</option>' +
+            '</select>' +
+            '</div>' +
+  
+            '</div>');
+    });
